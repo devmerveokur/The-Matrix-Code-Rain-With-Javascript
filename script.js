e996f5498eaa380aff05db3f4a568f0b2453d95c
Merve Okur <merveokur.dev@gmail.com>
2024-08-13 21:35:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const matrix = document.querySelector(".matrix");
+
+function makeId(){
+    let result = "";
+    let characters = "ABCDEFGHIJKLMNOPRSTUVYZWXabcdefghıjklmnoprstuvyzxw0123456789+/*-&をぁかさたなはまやわざだぱきちみりゐぎぢびすぬゆふゅるずえせけてねめれゑげゑおそのほを";
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+    return result;
+}
+function generateText(){
+    const w = 10;
+    const h = 25;
+
+for(let i = 0; i < w; i++)
+    {
+    const timeout = Math.floor(Math.random() * 10) * 1000;
+    let delay = 0;
+    const column = document.createElement("p");
+    for(let i = 0; i < h; i++){
+        const letter = document.createElement("span");
+        letter.innerHTML = makeId();
+        column.appendChild(letter);
+        delay += 100;
+        column.children[i].style.animationDelay = delay + "ms";
+    }
+    setTimeout(() => {
+        matrix.appendChild(column);
+    }, timeout);
+    } 
+}
+
+generateText();
